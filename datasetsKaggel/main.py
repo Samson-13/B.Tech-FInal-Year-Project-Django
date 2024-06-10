969b3bf797fabbf874c8ebc1676b91b90f53ad3c
Samson-13 <samsonbadding@gmail.com>
2024-06-10 19:11:19 +0530

    up

--- datasetsKaggel/main.py
+++ datasetsKaggel/main.py
@@ -3,11 +3,7 @@ import kaggle
 kaggle.api.authenticate()
 
 # kaggle.api.dataset_download_files('datamunge/sign-language-mnist', path='./datasetsKaggel', unzip=True)
-
-
-
-# kaggle.api.dataset_metadata('datamunge/sign-language-mnist', path='./datasetsKaggel')
-
+kaggle.api.dataset_download_files('ardamavi/sign-language-digits-dataset', path='./datasetsKaggel', unzip=True)
 
 print(kaggle.api.dataset_list_files('datamunge/sign-language-mnist').files)
 
